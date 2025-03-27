b0565c772ec0e3c4ad58a60524ab39592455eb7b
Ubuntu <ubuntu@ip-172-31-7-59.ap-south-1.compute.internal>
2025-03-27 14:46:57 +0000

    jenkins automation 5

--- hello.sh
+++ hello.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Hello, World! This is a build triggered by GitHub commit.
+echo "Hello, World! This is a build triggered by GitHub commit"
 echo "This is my first automation"
 echo "Changed from master branch to main branch"
 
